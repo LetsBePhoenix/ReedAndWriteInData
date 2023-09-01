2333a115e76735e4209ab0b8198c024f493569f3
Phoenix <110415215+LetsBePhoenix@users.noreply.github.com>
2023-09-01 13:18:48 +0200

    v.1.0

--- Program.cs
+++ Program.cs
@@ -1,7 +1,41 @@
 ﻿internal class Program
 {
+    public static int TestWert1 = 0;
+    public static int TestWert2 = 0;
+    public static string filePath = "Datei1.dat";
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Load();
+        Console.WriteLine(TestWert1 + "\n" + TestWert2);
+    }
+    private static void Save()
+    {
+        using (StreamWriter writer = new StreamWriter(filePath))
+        {
+            writer.Write(TestWert1);
+            writer.Write(',');
+            writer.Write(TestWert2);
+        }
+    }
+    private static void Load()
+    {
+        try
+        {
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    // Lade Zeilen
+                    string[] parts1 = line.Split(',');
+                    TestWert1 = Convert.ToInt32(parts1[0]);
+                    TestWert2 = Convert.ToInt32(parts1[1]);
+                }
+            }
+        }
+        catch
+        {
+            Save();
+        }
     }
 }
